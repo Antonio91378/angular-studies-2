1cd00f6d5ab2f05b49854b6004e5346ecc48af03
Antonio Dias <antoniovictor200@gmail.com>
2022-09-06 17:56:58 -0300

    Fazendo uma consulta get em uma api externa local pelo angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-
+import { HttpClient } from '@angular/common/http';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -7,14 +7,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'alurapic';
-  photos = [
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/5a/Sultan_the_Barbary_Lion.jpg/440px-Sultan_the_Barbary_Lion.jpg',
-      description: 'Leão',
-    },
-    {
-      url: 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/2d/Lioness_Etosha_NP.jpg/500px-Lioness_Etosha_NP.jpg',
-      description: 'Leoa',
-    },
-  ];
+  photos: Object[] = [];
+
+  constructor(http: HttpClient) {
+    const observable = http
+      .get<Object[]>('http://localhost:3000/flavio/photos')
+      .subscribe((photos) => (this.photos = photos));
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { PhotosModule } from './photos/photos.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PhotosModule],
+  imports: [BrowserModule, PhotosModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],
 })
