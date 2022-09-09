ff68cab35b590a64137b49a4ce8c6b4fbcd5a50c
Antonio Dias <antoniovictor200@gmail.com>
2022-09-09 15:24:24 -0300

    atualização de componentes e começando a aprender pipes no angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,11 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app.routing.module';
+import { ErrorsModule } from './errors/errors.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, PhotosModule, AppRoutingModule],
+  imports: [BrowserModule, PhotosModule, AppRoutingModule, ErrorsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/photos/photo-list/photo-list.component.ts
+++ src/app/photos/photo-list/photo-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Photo } from '../photo/photo';
 import { PhotoService } from '../photo/photo.service';
 
@@ -9,12 +10,17 @@ import { PhotoService } from '../photo/photo.service';
 })
 export class PhotoListComponent implements OnInit {
   photos: Photo[] = [];
+  filter: string = '';
 
-  constructor(private photoService: PhotoService) {}
+  constructor(
+    private photoService: PhotoService,
+    private activatedRoute: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const userName = this.activatedRoute.snapshot.params['userName'];
     this.photoService
-      .listFromUser('flavio')
+      .listFromUser(userName)
       .subscribe((photos) => (this.photos = photos));
   }
 }
