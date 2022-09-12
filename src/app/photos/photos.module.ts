a01bcca74a7f461362ac8853314a3c8a419d4a00
Antonio Dias <antoniovictor200@gmail.com>
2022-09-12 13:08:34 -0300

    checkpoint

--- src/app/photos/photos.module.ts
+++ src/app/photos/photos.module.ts
@@ -5,9 +5,16 @@ import { HttpClientModule } from '@angular/common/http';
 import { PhotoListComponent } from './photo-list/photo-list.component';
 import { PhotoFormComponent } from './photo-form/photo-form.component';
 import { PhotosComponent } from './photo-list/photos/photos.component';
+import { filterByDescription } from './photo-list/filter-by-description.pipe';
 
 @NgModule({
-  declarations: [PhotoComponent, PhotoListComponent, PhotoFormComponent, PhotosComponent],
+  declarations: [
+    PhotoComponent,
+    PhotoListComponent,
+    PhotoFormComponent,
+    PhotosComponent,
+    filterByDescription,
+  ],
   imports: [CommonModule, HttpClientModule],
   exports: [PhotoComponent, HttpClientModule],
 })
